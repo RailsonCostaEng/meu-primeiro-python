1ee29023bd0b0365c93e65cfcc0a8378dca44e30
RailsonCostaEng <railsoncostaeng@gmail.com>
2026-02-03 09:33:54 -0300

    Dia 02 criando meu gerenciador de tarefas com listas e loops

---
+++ tarefas.py
@@ -0,0 +1,8 @@
+lista_de_tarefas =[]
+print("---Meu gerenciador de tarefas---")
+while True:
+    tarefa = input("digite uma tarefa(ou 'sair'): ")
+    if tarefa == "sair":
+        break
+    lista_de_tarefas.append(tarefa)
+    print(f"Sua lista agora: {lista_de_tarefas} ")
